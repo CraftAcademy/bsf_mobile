b6dec5a46eab07cceba34742a4066a05386f9470
Jesper <jesper_gs_@hotmail.com>
2016-10-11 22:33:30 +0200

    refactored controllers and services

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,6 @@
 angular.module('slowfood.controllers', [])
 
-.controller('RestaurantsController', function($scope) {
-  $scope.helloRestaurant = 'Hello from Restaurant Controller';
-})
+.controller('RestaurantsController', function($scope, Restaurants) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+$scope.restaurants = Restaurants.all();
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,28 @@
 angular.module('slowfood.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Restaurants', function() {
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
+  var restaurants = [{
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: "Restaurant 1",
+    category: "Thai"
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: "Restaurant 2",
+    category: "Vegan"
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: "Restaurant 3",
+    category: "Pizza"
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: "Restaurant 4",
+    category: "Other"
   }];
 
   return {
     all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+      return restaurants;
     }
   };
 });
