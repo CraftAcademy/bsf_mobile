dbec7d424ab08273e2dcbfbc83e16511fe21f584
blokkinen <viktoria_blok@hotmail.com>
2016-10-12 11:47:11 +0200

    centered map in gothenburg

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('slowfood.controllers', [])
 .controller('RestaurantsController', function($scope, FakeRestaurants, uiGmapGoogleMapApi) {
 
     $scope.restaurants = FakeRestaurants.all();
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.map = { center: { latitude: 57.6945602, longitude: 11.9745962 }, zoom: 13 };
 
     uiGmapGoogleMapApi.then(function(maps) {
 
