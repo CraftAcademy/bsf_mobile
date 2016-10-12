aa48acf839e916317d748d60ae01eef8b197c337
thesuss <lullisuss@gmail.com>
2016-10-12 11:05:08 +0200

    config google maps in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 })
-
+.config(function(uiGmapGoogleMapApiProvider) {
+  uiGmapGoogleMapApiProvider.configure({
+    //    key: 'your api key',
+    v: '3.20', //defaults to latest 3.X anyhow
+    libraries: 'weather,geometry,visualization'
+  });
+})
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
