92f10d61204b215803f94f278d66aed3635d7d27
thesuss <lullisuss@gmail.com>
2016-10-12 11:13:12 +0200

    added gmap to module, added scope of map in controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,12 @@
 angular.module('slowfood.controllers', [])
 
-.controller('RestaurantsController', function($scope, FakeRestaurants) {
+.controller('RestaurantsController', function($scope, FakeRestaurants, uiGmapGoogleMapApi) {
 
-$scope.restaurants = FakeRestaurants.all();
+    $scope.restaurants = FakeRestaurants.all();
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+    uiGmapGoogleMapApi.then(function(maps) {
+
+    });
+
 });
