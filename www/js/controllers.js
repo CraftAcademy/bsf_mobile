a7f7731842ddfd268f1023f7a952e13042f24b33
thesuss <lullisuss@gmail.com>
2016-10-12 16:23:24 +0200

    Centers map on user

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,9 @@
 angular.module('slowfood.controllers', [])
 
-.controller('getRestaurantsCtrl', function($scope, restaurantsFactory, $ionicLoading, $ionicPopup) {
+.controller('getRestaurantsCtrl', function($scope, restaurantsFactory,
+                                           $ionicLoading, $ionicPopup, $cordovaGeolocation,
+                                           $ionicPlatform)
+{
     $scope.$on('$ionicView.enter', function () {
         $scope.getData();
     });
@@ -25,5 +28,23 @@ angular.module('slowfood.controllers', [])
             // Place some action here if needed...
         });
     };
-    $scope.map = { center: { latitude: 57.6945602, longitude: 11.9745962 }, zoom: 13 };
-})
+    $ionicPlatform.ready(function() {
+        $scope.map = { center: { latitude: 57.6945602, longitude: 11.9745962 }, zoom: 13 };
+
+        var posOptions = {
+            enableHighAccuracy: false,
+            timeout: 20000,
+            maximumAge: 0
+        };
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+            $scope.map = { center: { latitude: lat, longitude: long }, zoom: 13 };
+        }, function(err) {
+            $ionicLoading.hide();
+            console.log(err);
+        });
+
+    });
+
+});
