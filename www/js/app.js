a4a5a1ace2937ffa32aa7228e82fe8c1f8adf50c
Amber <amber@amberwilkie.com>
2016-10-13 15:06:34 +0200

    Routing and view for login page

--- www/js/app.js
+++ www/js/app.js
@@ -7,58 +7,67 @@
 // 'starter.controllers' is found in controllers.js
 
 angular.module('slowfood', ['ionic', 'slowfood.controllers', 'slowfood.services', 'ngResource', 'uiGmapgoogle-maps', 'ngCordova'])
-  .constant('API_URL', 'https://slow-food-august.herokuapp.com/api/v1')
+    .constant('API_URL', 'https://slow-food-august.herokuapp.com/api/v1')
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function(uiGmapGoogleMapApiProvider) {
-  uiGmapGoogleMapApiProvider.configure({
-    key: 'AIzaSyDksFPPj-FomswnVnuVeNMdGBYb1nA4pas',
-    v: '3.25', //defaults to latest 3.X anyhow
-    libraries: 'weather,geometry,visualization'
-  });
-})
-.config(function($stateProvider, $urlRouterProvider) {
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function (uiGmapGoogleMapApiProvider) {
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyDksFPPj-FomswnVnuVeNMdGBYb1nA4pas',
+            v: '3.25', //defaults to latest 3.X anyhow
+            libraries: 'weather,geometry,visualization'
+        });
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
 
-  // Each tab has its own nav history stack:
+            // Each tab has its own nav history stack:
 
-  .state('tab.restaurants', {
-    url: '/restaurants',
-    views: {
-      'tab-restaurants': {
-        templateUrl: 'templates/tab-restaurants.html',
-        controller: 'getRestaurantsCtrl'
-      }
-    }
-  });
+            .state('tab.restaurants', {
+                url: '/restaurants',
+                views: {
+                    'tab-restaurants': {
+                        templateUrl: 'templates/tab-restaurants.html',
+                        controller: 'getRestaurantsCtrl'
+                    }
+                }
+            })
+            .state('tab.login', {
+                url: '/login',
+                views: {
+                    'tab-login': {
+                        templateUrl: 'templates/tab-login.html',
+                        controller: 'loginCtrl'
+                    }
+                }
+            });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/restaurants');
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/restaurants');
 
-});
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,95 +1,99 @@
 angular.module('slowfood.controllers', [])
 
-.controller('getRestaurantsCtrl', function($scope, restaurantsFactory,
-                                           $ionicLoading, $ionicPopup, $cordovaGeolocation,
-                                           $ionicPlatform,
-                                           $ionicModal)
-{
+    .controller('getRestaurantsCtrl', function ($scope, restaurantsFactory,
+                                                $ionicLoading, $ionicPopup, $cordovaGeolocation,
+                                                $ionicPlatform,
+                                                $ionicModal) {
 
-    $scope.$on('$ionicView.enter', function () {
-        $scope.getData();
-    });
-    $scope.getData = function () {
-        $ionicLoading.show({
-            template: 'Retrieving data...'
+        $scope.$on('$ionicView.enter', function () {
+            $scope.getData();
         });
-        restaurantsFactory.get({}, function (response) {
-            $scope.restaurants = response.restaurants;
-            $ionicLoading.hide();
-        }, function (error) {
-            $ionicLoading.hide();
-            $scope.showAlert('Failure', error.statusText);
-        })
-    };
-    $scope.showAlert = function (message, content) {
-        var alertPopup = $ionicPopup.alert({
-            title: message,
-            template: content
-        });
-        alertPopup.then(function (res) {
-            // Place some action here if needed...
-        });
-    };
-
-    $scope.pickRestaurant = function(rest_id){
-        $ionicModal.fromTemplateUrl('templates/show-restaurant.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
-            $scope.openModal();
-        });
-
-        $scope.openModal = function () {
-            console.log('open modal method');
-            $scope.getRestaurantData(rest_id);
-            $scope.modal.show();
-        };
-
-        $scope.closeModal = function() {
-            $scope.modal.hide();
-        };
-
-        //Cleanup modal after we're done using it
-        $scope.$on('$destroy', function() {
-            $scope.modal.remove();
-        });
-        $scope.$on('modal.removed', function() {
-            // some action to perform after we hide the modal
-        });
-
-        $scope.getRestaurantData = function (rest_id) {
+        $scope.getData = function () {
             $ionicLoading.show({
                 template: 'Retrieving data...'
             });
-            restaurantsFactory.query({id: rest_id}, function (response) {
-                $scope.restaurant = response;
-                console.log(response);
+            restaurantsFactory.get({}, function (response) {
+                $scope.restaurants = response.restaurants;
                 $ionicLoading.hide();
             }, function (error) {
                 $ionicLoading.hide();
                 $scope.showAlert('Failure', error.statusText);
             })
         };
-    };
-
-    $ionicPlatform.ready(function() {
-        $scope.map = { center: { latitude: 57.6945602, longitude: 11.9745962 }, zoom: 13 };
-
-        var posOptions = {
-            enableHighAccuracy: false,
-            timeout: 20000,
-            maximumAge: 0
+        $scope.showAlert = function (message, content) {
+            var alertPopup = $ionicPopup.alert({
+                title: message,
+                template: content
+            });
+            alertPopup.then(function (res) {
+                // Place some action here if needed...
+            });
         };
-        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-            var lat  = position.coords.latitude;
-            var long = position.coords.longitude;
-            $scope.map = { center: { latitude: lat, longitude: long }, zoom: 13 };
-        }, function(err) {
-            $ionicLoading.hide();
-            console.log(err);
+
+        $scope.pickRestaurant = function (rest_id) {
+            $ionicModal.fromTemplateUrl('templates/show-restaurant.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                $scope.modal = modal;
+                $scope.openModal();
+            });
+
+            $scope.openModal = function () {
+                console.log('open modal method');
+                $scope.getRestaurantData(rest_id);
+                $scope.modal.show();
+            };
+
+            $scope.closeModal = function () {
+                $scope.modal.hide();
+            };
+
+            //Cleanup modal after we're done using it
+            $scope.$on('$destroy', function () {
+                $scope.modal.remove();
+            });
+            $scope.$on('modal.removed', function () {
+                // some action to perform after we hide the modal
+            });
+
+            $scope.getRestaurantData = function (rest_id) {
+                $ionicLoading.show({
+                    template: 'Retrieving data...'
+                });
+                restaurantsFactory.query({id: rest_id}, function (response) {
+                    $scope.restaurant = response;
+                    console.log(response);
+                    $ionicLoading.hide();
+                }, function (error) {
+                    $ionicLoading.hide();
+                    $scope.showAlert('Failure', error.statusText);
+                })
+            };
+        };
+
+        $ionicPlatform.ready(function () {
+            $scope.map = {center: {latitude: 57.6945602, longitude: 11.9745962}, zoom: 13};
+
+            var posOptions = {
+                enableHighAccuracy: false,
+                timeout: 20000,
+                maximumAge: 0
+            };
+            $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+                var lat = position.coords.latitude;
+                var long = position.coords.longitude;
+                $scope.map = {center: {latitude: lat, longitude: long}, zoom: 13};
+            }, function (err) {
+                $ionicLoading.hide();
+                console.log(err);
+            });
+
         });
+
+    })
+
+    .controller('loginCtrl', function () {
 
     });
 
-});
