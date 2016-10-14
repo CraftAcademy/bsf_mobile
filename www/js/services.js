2c354853b29f2ab6a9cd187b64d004161afd84bd
Amber <amber@amberwilkie.com>
2016-10-14 15:25:20 +0200

    Create two add-to-cart methods, connect to buttons

--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,21 @@
 angular.module('slowfood.services', [])
 
-.factory('restaurantsFactory', function ($resource, API_URL) {
-    return $resource(API_URL + '/restaurants/:id', {},
-        {
-            'query': {
-                method: 'GET',
-                isArray: false
+    .factory('restaurantsFactory', function ($resource, API_URL) {
+        return $resource(API_URL + '/restaurants/:id', {},
+            {
+                'query': {
+                    method: 'GET',
+                    isArray: false
+                }
+            })
+    })
+    .factory('cartsFactory', function ($resource, API_URL) {
+        return $resource(API_URL + '/carts', {}, {
+            'post': {
+                method: 'POST'
+            },
+            'put': {
+                method: 'PUT'
             }
         })
-})
+    });
